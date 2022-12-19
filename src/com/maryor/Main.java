acc83fc808b690e0093fb210faae53e2ec23b4c7
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-19 21:40:05 +0100

    Update Main.java

--- src/com/maryor/Main.java
+++ src/com/maryor/Main.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class Main {
+package com.maryor;
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank("Nation australia bank");
+        if (bank.addBranch("Adelaide")){
+            System.out.println("Branch available");
+        }
+        bank.addCustomer("Adelaide", "Maryor", 1000000.0);
+        bank.addCustomer("Adelaide", "Mary", 1050000.0);
+        bank.addCustomer("Adelaide", "May", 10043200.0);
+
+        bank.addBranch("Sydney");
+        bank.addCustomer("Sydney", "bob", 15697.7);
+
+        bank.addCustomerTransaction("Adelaide", "Maryor", 645789.67);
+        bank.addCustomerTransaction("Adelaide", "Mary", 64578009.67);
+        bank.addCustomerTransaction("Adelaide", "Mayo", 6578900.67);
+
+        bank.listCustomer("Adelaide", true);
+        bank.listCustomer("Sydney", true);
+
+        bank.addBranch("Melbourne");
+        if (!bank.addCustomer("Melbourne", "Brian", 5.5)) {
+            System.out.println("branch doesnt exist");
+        }
+        bank.listCustomer("Melbourne", true);
+
+        if(!bank.addBranch("Adelaide")) {
+            System.out.println("branch already exist");
+        }
+
+        if (!bank.addCustomerTransaction("Adelaide", "Fergus", 676.8)) {
+            System.out.println("Customer does not exist at the branch");
+        }
+
+    }
+
 }
